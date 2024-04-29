844b0b84a283627fc888efd256a923766ad75ec1
ErenOzilgili <erenozilgili@gmail.com>
2024-04-29 11:58:55 +0300

    Chatable interface --> FriendChat and ActivityChat classes created.

---
+++ FriendChat.java
@@ -0,0 +1,13 @@
+public class FriendChat implements Chatable{
+    public void getChat(Object obj){
+        Account acc = (Account)obj;
+
+
+    }
+
+    public void sendMessage(Object obj){
+        Account acc = (Account)obj;
+        
+    }
+
+}
