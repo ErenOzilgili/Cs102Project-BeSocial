daf8974835733d5518f081d17d830b5ebd6bd94f
Ahmet Eren Gözübenli <gozubenli04@gmail.com>
2024-04-29 12:23:37 +0300

    Auto stash before merge of "main" and "origin/main"

--- MainManager.java
+++ MainManager.java
@@ -1,17 +1,18 @@
 public class MainManager {
 
+    public static Database1 db; 
     public static void main(String[] args){
         // TODO code application logic here
         
-        // db = new Database1();        
-        // db.crateConnection();
-        DBConnection.createConnection();
-
-        LogInFrame logIn = new LogInFrame();
-        logIn.setVisible(true);
-        logIn.pack();
-        logIn.setLocationRelativeTo(null);
+        db = new Database1();        
+        db.crateConnection();
         
+        NewJFrame login = new NewJFrame(); // Creating frame that I implement in NewJFrame class
+        login.setVisible(true);
+        login.pack();
+        login.setLocationRelativeTo(null); //center
+        
+        // I try to connect the databse with our java program
     }
-    protected static Database1 db; 
+
 }
