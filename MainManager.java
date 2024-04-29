48ed55a3a22ca4efb9f9ec3b5b52c75edc0f403a
Mehmet Efe Mutlu <mehmetefemutlu7@gmail.com>
2024-04-29 14:21:59 +0300

    notification class

--- MainManager.java
+++ MainManager.java
@@ -3,21 +3,19 @@ import java.util.ArrayList;
 public class MainManager {
 
     public static Database1 db; 
-    
+    public static Account user;
+    public static ArrayList<Account> allUsers;
+    public static ArrayList<Activity>allActivities;
     public static void main(String[] args){
         // TODO code application logic here
         
         db = new Database1();        
         db.crateConnection();
         
-        
-
         NewJFrame login = new NewJFrame(); // Creating frame that I implement in NewJFrame class
         login.setVisible(true);
         login.pack();
         login.setLocationRelativeTo(null); //center
-
-
         
         // I try to connect the databse with our java program
     }
--- Notification.java
+++ Notification.java
@@ -1,3 +1,36 @@
+import java.sql.*;
+
 public class Notification {
     
+    private static enum Type{
+        ACTIVITY, FRIEND;
+    }
+    private int notID;
+    private Type type;
+    private String description;
+    private Account senderAccount;
+    private Account receiverAccount;
+
+    public Notification(){
+
+    }
+
+    public static void getNotification(){
+        try{
+            Statement stat = MainManager.db.getCon().createStatement();
+            ResultSet set = stat.executeQuery("SELECT * FROM notifications WHERE receiverID=%d".formatted(MainManager.user.getID()));
+            while(set.next()){
+            }
+        }
+        catch(SQLException ex){
+            ex.getMessage();
+        }
+    }
+
+    public static void deleteNotification(){
+        try{
+
+        }
+    }
+
 }
