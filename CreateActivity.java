1d8bf20bb82738b16086d4084252f7e0622a12f0
Burak Tutuş <yburaktutus@gmail.com>
2024-04-29 15:02:28 +0300

    solve problems about main

--- CreateActivity.java
+++ CreateActivity.java
@@ -1,8 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
-package loginandsignup;
 
 /**
  *
@@ -367,7 +362,7 @@ public class CreateActivity extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    public static void createCreateActivityPanel() {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
